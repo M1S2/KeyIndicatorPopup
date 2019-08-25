6ab6e044a845507a165f79f43bdac52bc0acd229
Markus Scheich <markus.scheich@gmx.de>
2019-08-25 20:24:04 +0200

    GitHubRepo attribute added to AssemblyInfo.cs

--- KeyIndicatorPopup/Properties/AssemblyInfo.cs
+++ KeyIndicatorPopup/Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyInfoHelper.GitHubReleases.GitHubRepo("https://github.com/M1S2/KeyIndicatorPopup")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
